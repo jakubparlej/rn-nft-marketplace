f242b8e0979e9f3df4f32f807b5ff4aa89e3261a
Jakub Parlej <jakubparlej@gmail.com>
2022-05-06 09:19:40 +0200

    initial commit

---
+++ components/index.js
@@ -0,0 +1,9 @@
+import HomeHeader from './HomeHeader';
+import NFTCard from './NFTCard';
+import FocusedStatusBar from './FocusedStatusBar';
+import { CircleButton, RectButton } from './Button';
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+import { SubInfo } from './SubInfo';
+
+export { HomeHeader, NFTCard, FocusedStatusBar, SubInfo, CircleButton, RectButton, DetailsBid, DetailsDesc }
